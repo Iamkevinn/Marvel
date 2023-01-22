12be864de77ad992fa7ae6e5045254d7e42e5f64
Kevin Pedraza <kevinpedraza2003@gmail.com>
2023-01-22 13:17:12 -0500

    Actualizando Barra de Navegacion

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -6,16 +6,7 @@ window.addEventListener("load", function() {
   //Funcion para cargar una animación despues de 3500 microsegundos
 window.onload = function() {
     setTimeout(function(){
-        document.querySelector('.Button-Menu').classList.add('Opacity');
-    }, 3500);
+        document.querySelector('.Container-Nav').classList.add('Opacity');
+    }, 3250);
 };
-function Menu(){
-    document.querySelector('#Menuid').classList.add('Display');
-    document.querySelector('#Characters').classList.add('Display');
-    document.querySelector('#Time').classList.add('Display');
-    document.querySelector('#Questions').classList.add('Display');
-    document.querySelector('.Nav').classList.add('Background');
-    document.getElementById('Icon-C').style.margin = "0 0 0 30px";
-    document.getElementById('Icon-T').style.margin = "0 0 0 30px";
-    document.getElementById('Icon-Q').style.margin = "0 0 0 15px";
-}
+
