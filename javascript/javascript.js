390f244cec1f89654f994cd0641117cf262cefe2
Kevin Pedraza <kevinpedraza2003@gmail.com>
2023-01-23 14:55:31 -0500

    Flecha agr - Error titulo crr - Nav selec agr

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,13 +2,21 @@
 window.addEventListener("load", function() {
     document.querySelector('.Marvel-Logo').classList.add('Opacity');
   });
-  //Funcion para cargar una animación despues de 3500 microsegundos
+  window.addEventListener("scroll", function() {
+    document.querySelector('#ArrowDown').classList.add('NoOpacity');
+  });
+  //Funcion para cargar una animación despues de cierto tiempo
 window.onload = function() {
     setTimeout(function(){
         document.querySelector('.Container-Nav').classList.add('Opacity');
     }, 3250);
 };
 function Characters(){
+    document.getElementById('Questions').style.backgroundColor = "transparent";
+    document.getElementById('Cronology').style.backgroundColor = "transparent";
+    document.getElementById('Characters').style.backgroundColor = "#343434";
+    document.querySelector('#ArrowDown').classList.add('Opacity');
+    document.querySelector('#ArrowDown').classList.remove('NoOpacity');
     fetch('Characters.html')
     .then(response => response.text())
     .then(data => {
@@ -18,17 +26,26 @@ function Characters(){
     .catch(error => console.log(error));
 }
 function Cronology(){
+    document.getElementById('Questions').style.backgroundColor = "transparent";
+    document.getElementById('Characters').style.backgroundColor = "transparent";
+    document.getElementById('Cronology').style.backgroundColor = "#343434";
+    document.querySelector('#ArrowDown').classList.add('Opacity');
+    document.querySelector('#ArrowDown').classList.remove('NoOpacity');
     fetch('Cronology.html')
     .then(response => response.text())
     .then(data => {
         let container = document.getElementById('Section');
-        
         container.innerHTML = data;
     })
     .catch(error => console.log(error));
 
 }
 function Questions(){
+    document.getElementById('Cronology').style.backgroundColor = "transparent";
+    document.getElementById('Characters').style.backgroundColor = "transparent";
+    document.getElementById('Questions').style.backgroundColor = "#343434";
+    document.querySelector('#ArrowDown').classList.add('Opacity');
+    document.querySelector('#ArrowDown').classList.remove('NoOpacity');
     fetch('Questions.html')
     .then(response => response.text())
     .then(data => {
