033dbec5e04a6491e41a5feebf5b5166a1d4c9a4
Kevin Pedraza <kevinpedraza2003@gmail.com>
2023-01-24 12:22:35 -0500

    Agregado animacion scroll, cambiado iconos de nav

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,41 +1,46 @@
+let seccion = document.getElementById("Section");
 //Funcion para cargar algo despues de cargar la pagina
+window.addEventListener("DOMContentLoaded", function() {
+
+});
 window.addEventListener("load", function() {
     document.querySelector('.Marvel-Logo').classList.add('Opacity');
-  });
-  window.addEventListener("scroll", function() {
-    document.querySelector('#ArrowDown').classList.add('NoOpacity');
-  });
+});
   //Funcion para cargar una animación despues de cierto tiempo
 window.onload = function() {
     setTimeout(function(){
         document.querySelector('.Container-Nav').classList.add('Opacity');
-    }, 3250);
+    }, 2050);
 };
+function ScrollSection(){
+    window.scrollTo({
+        top: seccion.offsetTop,
+        behavior: 'smooth'
+    });
+}
 function Characters(){
     document.getElementById('Questions').style.backgroundColor = "transparent";
     document.getElementById('Cronology').style.backgroundColor = "transparent";
-    document.getElementById('Characters').style.backgroundColor = "#343434";
-    document.querySelector('#ArrowDown').classList.add('Opacity');
-    document.querySelector('#ArrowDown').classList.remove('NoOpacity');
+    document.getElementById('Characters').style.backgroundColor = "#212121";
     fetch('Characters.html')
     .then(response => response.text())
     .then(data => {
         let container = document.getElementById('Section');
         container.innerHTML = data;
+        ScrollSection();
     })
     .catch(error => console.log(error));
 }
 function Cronology(){
     document.getElementById('Questions').style.backgroundColor = "transparent";
     document.getElementById('Characters').style.backgroundColor = "transparent";
-    document.getElementById('Cronology').style.backgroundColor = "#343434";
-    document.querySelector('#ArrowDown').classList.add('Opacity');
-    document.querySelector('#ArrowDown').classList.remove('NoOpacity');
+    document.getElementById('Cronology').style.backgroundColor = "#212121";
     fetch('Cronology.html')
     .then(response => response.text())
     .then(data => {
         let container = document.getElementById('Section');
         container.innerHTML = data;
+        ScrollSection();
     })
     .catch(error => console.log(error));
 
@@ -43,14 +48,13 @@ function Cronology(){
 function Questions(){
     document.getElementById('Cronology').style.backgroundColor = "transparent";
     document.getElementById('Characters').style.backgroundColor = "transparent";
-    document.getElementById('Questions').style.backgroundColor = "#343434";
-    document.querySelector('#ArrowDown').classList.add('Opacity');
-    document.querySelector('#ArrowDown').classList.remove('NoOpacity');
+    document.getElementById('Questions').style.backgroundColor = "#212121";
     fetch('Questions.html')
     .then(response => response.text())
     .then(data => {
         let container = document.getElementById('Section');
         container.innerHTML = data;
+        ScrollSection();
     })
     .catch(error => console.log(error));
 }
