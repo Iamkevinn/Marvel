17af31ce33fc3f31d48516378b38ae98fdda5007
Kevin Pedraza <kevinpedraza2003@gmail.com>
2023-01-25 13:30:28 -0500

    movies delete - nav update - comments agregred

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,23 +1,48 @@
+//get variables
 let seccion = document.getElementById("Section");
-//Funcion para cargar algo despues de cargar la pagina
+let scrollinYprev = window.pageYOffset;
+let scrollinYcurrent = window.pageXOffset;
+let navbar = document.querySelector('.Nav');
+//function for hide the nav while the user make scroll to bottom
+window.addEventListener('scroll',function(){
+    //Update the position of the scroll
+    scrollinYcurrent = window.pageYOffset;
+
+    //check if the user is scrolling to the top
+    if(scrollinYprev < scrollinYcurrent){
+        navbar.classList.add('Display');
+    } else {
+        navbar.classList.remove('Display');
+    }
+    scrollinYprev = scrollinYcurrent;
+})
+
+//Function for load something after load the page
 window.addEventListener("DOMContentLoaded", function() {
 
 });
+
+//function for load something in any moment?
 window.addEventListener("load", function() {
     document.querySelector('.Marvel-Logo').classList.add('Opacity');
 });
-  //Funcion para cargar una animación despues de cierto tiempo
+
+//Function for load something in a time predeterminate after load the page
 window.onload = function() {
     setTimeout(function(){
         document.querySelector('.Container-Nav').classList.add('Opacity');
     }, 2050);
 };
+
+//Function for animation to select any boton of the nav
 function ScrollSection(){
     window.scrollTo({
         top: seccion.offsetTop,
         behavior: 'smooth'
     });
 }
+
+//Function of Section of Characters
 function Characters(){
     document.getElementById('Questions').style.backgroundColor = "transparent";
     document.getElementById('Cronology').style.backgroundColor = "transparent";
@@ -30,6 +55,8 @@ function Characters(){
     })
     .catch(error => console.log(error));
 }
+
+//Function of section of Cronology
 function Cronology(){
     document.getElementById('Questions').style.backgroundColor = "transparent";
     document.getElementById('Characters').style.backgroundColor = "transparent";
@@ -43,6 +70,8 @@ function Cronology(){
     .catch(error => console.log(error));
 
 }
+
+//Function of section of Questions
 function Questions(){
     document.getElementById('Cronology').style.backgroundColor = "transparent";
     document.getElementById('Characters').style.backgroundColor = "transparent";
